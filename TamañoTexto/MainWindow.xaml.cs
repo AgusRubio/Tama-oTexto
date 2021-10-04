7cb8aa6c3b2adaf0b4fb5c794f28169786d97f82
Agustin Rubio <agustin.rubio16@iesdoctorbalmis.com>
2021-10-04 13:29:45 +0200

    Agregar archivos de proyecto.

---
+++ TamañoTexto/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TamañoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MedianoRadioButton.IsChecked = true;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton tamañoTexto = sender as RadioButton;
+            TextoTextBlock.FontSize = int.Parse(tamañoTexto.Tag.ToString()); //Hacemos un parse de los datos en string para pasar el valor a el cuadro de texto.
+        }
+    }
+}
