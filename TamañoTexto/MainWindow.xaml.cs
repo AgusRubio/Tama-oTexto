4c2fb67cae2974ec9c10b4590a81a180a7ff6146
Agustin Rubio <agustin.rubio16@iesdoctorbalmis.com>
2021-10-04 13:38:20 +0200

    Actualizo uno de los nombres porque no era correcto.

--- TamañoTexto/MainWindow.xaml.cs
+++ TamañoTexto/MainWindow.xaml.cs
@@ -23,13 +23,13 @@ namespace TamañoTexto
         public MainWindow()
         {
             InitializeComponent();
-            MedianoRadioButton.IsChecked = true;
+            medianoRadioButton.IsChecked = true;
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            RadioButton tamañoTexto = sender as RadioButton;
-            TextoTextBlock.FontSize = int.Parse(tamañoTexto.Tag.ToString()); //Hacemos un parse de los datos en string para pasar el valor a el cuadro de texto.
+            RadioButton tamañoTexto = (RadioButton)sender;
+            TextoTextBlock.FontSize = double.Parse(tamañoTexto.Tag.ToString()); //Hacemos un parse de los datos en string para pasar el valor a el cuadro de texto.
         }
     }
 }
